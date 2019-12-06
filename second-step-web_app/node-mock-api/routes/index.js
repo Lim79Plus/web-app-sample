a74f49e1297412049483db7503002335f9e7ea25
YFT <machbuilders@gmail.com>
2019-12-06 16:30:40 +0900

    Create Node mock api

--- second-step-web_app/node-mock-api/routes/index.js
+++ second-step-web_app/node-mock-api/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const fs = require('fs');
+const tweets = JSON.parse(fs.readFileSync('tweets.json'));
+
+router.get('/msg/max', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({max:tweets.length}));
+});
+
+router.get('/msg', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(tweets));
+});
+
+router.get('/msg/:from(\\d+)-:to(\\d+)', function (req, res, next) {
+    const minIndex = 0;
+    const maxIndex = tweets.length;
+
+    let from = parseInt(req.params.from) - 1;
+    let to = parseInt(req.params.to);
+    from = Math.min(from, maxIndex);
+    from = Math.max(from, minIndex);
+    to = Math.min(to, maxIndex);
+    to = Math.max(to, minIndex);
+    from = Math.min(from, to);
+
+    const result = [...tweets].reverse().splice(from, to - from);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
 });
 
 module.exports = router;
